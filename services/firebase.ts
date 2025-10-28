a5e620e66765d887f87a065dab1d67a80fc4fd2c
bvnkame <bvnbase@gmail.com>
2025-10-28 17:43:32 +0700

    Add env

--- services/firebase.ts
+++ services/firebase.ts
@@ -17,13 +17,13 @@ import { getAnalytics } from "firebase/analytics";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyD-S4Jp1fC-mJx_SX18So63ArLIIIT1I8Q",
-  authDomain: "signerapp-87443.firebaseapp.com",
-  projectId: "signerapp-87443",
-  storageBucket: "signerapp-87443.firebasestorage.app",
-  messagingSenderId: "577481581859",
-  appId: "1:577481581859:web:5dc479e9ab4ad05c399896",
-  measurementId: "G-EYLK8MQCL3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
